79893a477e78c6b56c725293dae7d4bd07181998
Codi <cknerr92@gmail.com>
2023-08-12 16:56:16 -0500

    Create edit button and fix display of todo items

--- gui.py
+++ gui.py
@@ -3,24 +3,41 @@ import PySimpleGUI as sg
 
 # Creating components for window
 label = sg.Text("Add an item to you todo list:")
-input_box = sg.InputText(tooltip="Enter todo item", key="todo")
+input_box = sg.InputText(tooltip="Enter todo item", key='todo')
 add_button = sg.Button("Add")
+todo_list_box = sg.Listbox(values=Functions.get_todo_list(), key='todo_items',
+                           enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
 # Creating window and displaying it
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button],
+                   [todo_list_box, edit_button]],
                    font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['todo_items'])
     match event:
         case "Add":
             todo_list = Functions.get_todo_list()
             new_todo = values['todo'] + "\n"
             todo_list.append(new_todo)
             Functions.write_todo_list(todo_list)
+            window['todo_items'].update(values=todo_list)
+        case "Edit":
+            edit_todo = values['todo_items'][0]
+            new_todo = values['todo']
+
+            todo_list = Functions.get_todo_list()
+            index = todo_list.index(edit_todo)
+            todo_list[index] = new_todo
+            Functions.write_todo_list(todo_list)
+            window['todo_items'].update(values=todo_list)
+        case 'todo_items':
+            window['todo'].update(value=values['todo_items'][0])
         case sg.WIN_CLOSED:
             break
 window.close()
